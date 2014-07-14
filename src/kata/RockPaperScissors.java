d95ae468dadb96f6de2177e2bb97942dc3ef8ee7
da101025 <daniel.p.jones@gmail.com>
2014-07-14 17:25:34 -0600

    Paper Rock Scissors

--- src/kata/RockPaperScissors.java
+++ src/kata/RockPaperScissors.java
@@ -13,30 +13,23 @@ public class RockPaperScissors {
         System.out.println("What's your move?\np) Paper; r) Rock; s) Scissors: ");
         String input = scan.nextLine().toLowerCase();
         String valid = "rps";
+        
         if(!valid.contains(input)) {
-            
-            throw new RuntimeException("Not a valid input. Game over!");
-            
+            throw new RuntimeException("Not a valid input. Game over!");    
         }
+        
         RPS selection = RPS.ROCK;
-        if(!input.equalsIgnoreCase("r")) {
-            
+        if(!input.equalsIgnoreCase("r")) {    
             selection = (input.equals("p")) ? RPS.PAPER : RPS.SCISSORS;
-            
         }
         
         System.out.println("Player 2 chose " + RPS.ROCK);
-        
         int result = battle(selection, RPS.ROCK);
-        
         if(result > 0) {
-            
             System.out.printf("Player %d is the winner!", result);
         } else {
-            
             System.out.printf("Draw! No winner this round.", result);
-        }
-        
+        } 
         
     }
     
@@ -44,6 +37,7 @@ public class RockPaperScissors {
        
         //default the winner to player 2
         int winner = 2;
+        
         Map<RPS, RPS> winnerGrid = new HashMap<>();
         winnerGrid.put(RPS.ROCK, RPS.SCISSORS);
         winnerGrid.put(RPS.PAPER, RPS.ROCK);
@@ -51,19 +45,15 @@ public class RockPaperScissors {
         
         //throw an error if we get bad inputs
         if(player1 == null || player2 == null) {  
-        
             throw new RuntimeException("Inputs cannot be null");
-            
         }
         
         //return a 0 for "Draw" if we get the same inputs
         if(player1 == player2) { return 0; }
         
-        //determine if player 1 wins, else we assume player 2 does
+        //determine if player 1 wins, else by default player 2 does
         if(winnerGrid.get(player1).equals(player2)) {
-            
             winner = 1;
-            
         }
         
         return winner;
